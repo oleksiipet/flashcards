89b900719b981b2185352613d20afba7184db6e6
oleksii <aleksey.petenko@gmail.com>
2018-12-12 23:55:48 +0200

    Part three implementation

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,7 +1,34 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.printf("Input the number of cards:\n");
+
+      int numCards = Integer.parseInt(scanner.nextLine());
+
+      String[] cards = new String[numCards];
+      String[] definitions = new String[numCards];
+
+      for (int i = 0; i < numCards; i++) {
+        System.out.printf("The card #%d:\n", i + 1);
+        cards[i] = scanner.nextLine();
+        System.out.printf("The definition of the card #%d:\n", i + 1);
+        definitions[i] = scanner.nextLine();
+      }
+
+      for (int i = 0; i < numCards; i++) {
+        System.out.printf("Print the definition of \"%s\":\n", cards[i]);
+        String guess = scanner.nextLine();
+        if (definitions[i].equals(guess)) {
+          System.out.printf("Correct answer. ");
+        } else {
+          System.out.printf("Wrong answer (the correct one is \"%s\").", definitions[i]);
+        }
+      }
     }
+  }
 }
