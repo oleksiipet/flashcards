afdf8712ba1456134a4c6efd3f399a49937c4a25
oleksii <aleksey.petenko@gmail.com>
2018-12-13 00:45:28 +0200

    Part four implementation

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,5 +1,8 @@
 package flashcards;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -10,23 +13,30 @@ public class Main {
 
       int numCards = Integer.parseInt(scanner.nextLine());
 
-      String[] cards = new String[numCards];
-      String[] definitions = new String[numCards];
+      Map<String, String> cardToDefinition = new LinkedHashMap<>();
+      Map<String, String> definitionToCard = new LinkedHashMap<>();
 
       for (int i = 0; i < numCards; i++) {
         System.out.printf("The card #%d:\n", i + 1);
-        cards[i] = scanner.nextLine();
+        String card = scanner.nextLine();
         System.out.printf("The definition of the card #%d:\n", i + 1);
-        definitions[i] = scanner.nextLine();
+        String definition = scanner.nextLine();
+        cardToDefinition.put(card, definition);
+        definitionToCard.put(definition, card);
       }
 
-      for (int i = 0; i < numCards; i++) {
-        System.out.printf("Print the definition of \"%s\":\n", cards[i]);
+      for (String card : cardToDefinition.keySet()) {
+        System.out.printf("Print the definition of \"%s\":\n", card);
         String guess = scanner.nextLine();
-        if (definitions[i].equals(guess)) {
+        if (definitionToCard.containsKey(guess) && cardToDefinition.get(card).equals(guess)) {
           System.out.printf("Correct answer. ");
+        } else if (definitionToCard.containsKey(guess)) {
+          System.out.printf(
+              "Wrong answer (the correct one is \"%s\", you've just written a definition of \"%s\" card). ",
+              cardToDefinition.get(card), definitionToCard.get(guess));
         } else {
-          System.out.printf("Wrong answer (the correct one is \"%s\").", definitions[i]);
+          System.out
+              .printf("Wrong answer (the correct one is \"%s\").", cardToDefinition.get(card));
         }
       }
     }
